d034afab4ab8a504a78b5f6d61e5ee669d3fd646
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2020-06-22 08:55:15 +0200

    First Launch :D Button class added Textbox Class added

---
+++ Button.h
@@ -0,0 +1,92 @@
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Button {
+public:
+    Button(std::string btnText, sf::Vector2f buttonSize, int charSize, sf::Color bgColor, sf::Color textColor) {
+        button.setSize(buttonSize);
+        button.setFillColor(bgColor);
+
+        // Get these for later use:
+        btnWidth = buttonSize.x;
+        btnHeight = buttonSize.y;
+
+        text.setString(btnText);
+        text.setCharacterSize(charSize);
+        text.setFillColor(textColor);
+    }
+    Button(sf::Texture *texture){
+        button.setTexture(texture);
+
+        button.setSize({(float)texture->getSize().x,(float)texture->getSize().y});
+        btnHeight = texture->getSize().x;
+        btnWidth = texture->getSize().y;
+    }
+
+    // Pass font by reference:
+    void setFont(sf::Font &fonts) {
+        text.setFont(fonts);
+    }
+
+    void setBackColor(sf::Color color) {
+        button.setFillColor(color);
+    }
+
+    void setText(std::string txt,int char_size,sf::Color fillcolor){
+        text.setString(txt);
+        text.setCharacterSize(char_size);
+        text.setFillColor(fillcolor);
+    }
+    void setTextColor(sf::Color color) {
+        text.setFillColor(color);
+    }
+    void setScale(sf::Vector2f scale){
+        button.setScale(scale);
+    }
+
+    void setPosition(sf::Vector2f point) {
+        button.setPosition(point);
+
+        // Center text on button:
+        float xPos = (point.x + btnWidth / 2) - (text.getLocalBounds().width / 2);
+        float yPos = (point.y + btnHeight / 2.2) - (text.getLocalBounds().height / 2);
+        text.setPosition(xPos, yPos);
+    }
+
+    void drawTo(sf::RenderWindow &window) {
+        window.draw(button);
+        window.draw(text);
+    }
+
+    // Check if the mouse is within the bounds of the button:
+    bool isMouseOver(sf::RenderWindow &window) {
+        int mouseX = sf::Mouse::getPosition(window).x;
+        int mouseY = sf::Mouse::getPosition(window).y;
+
+        int btnPosX = button.getPosition().x;
+        int btnPosY = button.getPosition().y;
+
+        int btnxPosWidth = button.getPosition().x + btnWidth;
+        int btnyPosHeight = button.getPosition().y + btnHeight;
+
+        if (mouseX < btnxPosWidth && mouseX > btnPosX && mouseY < btnyPosHeight && mouseY > btnPosY) {
+            return true;
+        }
+        return false;
+    }
+    bool mosuein(sf::RenderWindow &window){
+        sf::FloatRect rect = button.getGlobalBounds();
+        sf::Vector2f mouse =(sf::Vector2f) sf::Mouse::getPosition(window);
+        return rect.contains(mouse);
+    }
+private:
+    sf::RectangleShape button;
+    sf::Text text;
+
+    int btnWidth;
+    int btnHeight;
+};
--- main.cpp
+++ main.cpp
@@ -1,31 +1,66 @@
 /*---------BINGO---------*/
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include"Button.h"
 
 using namespace std;
 using namespace sf;
 
 int main(){
-    RenderWindow mainwindow(VideoMode(800, 700), "PENGO", Style::Close);
-    int x=0,y=x,z=y;
-    float angle = 0;
-    bool rotateposs=1;
-    while (mainwindow.isOpen()){//the main game loop
+    //-------- start window data -------//
+    RenderWindow start_window(VideoMode(700,700),"Start Window",Style::Close);
+    start_window.setPosition(Vector2i(0,0));
+    Texture background_texture,start_btn_texture;
+    //background_texture.loadFromFile("full_window.jpg");// to test positions
+    background_texture.loadFromFile("background.jpg");
+    start_btn_texture.loadFromFile("start_btn.png");
+    RectangleShape background;
+    background.setTexture(&background_texture);
+    background.setPosition(0,0);
+    background.setSize(Vector2f(background_texture.getSize()));
+    Button start_btn(&start_btn_texture);
+    start_btn.setPosition({240, 196});
+    //start_btn.setBackColor(Color(255,255,255,100));
+    while(start_window.isOpen()){//strat window loop
+
+        Event ev;
+        while(start_window.pollEvent(ev))
+            switch (ev.type) {//even handle switch case
+            case Event::Closed:
+                start_window.close();break;
+            case Event::MouseButtonPressed://to know the positions on the screen
+                cout<<Mouse::getPosition().x<<" "<<Mouse::getPosition().y<<endl;
+                if(start_btn.mosuein(start_window))
+                    start_btn.setScale({0.9,0.9});
+                break;
+            case Event::MouseButtonReleased:
+                if(start_btn.mosuein(start_window))
+                    start_btn.setScale({1,1});
+            }
+        start_window.draw(background);
+        start_btn.drawTo(start_window);
+        start_window.display();
+    }
+//RenderWindow mainwindow(VideoMode(800, 700), "PENGO", Style::Close);
+    //mainwindow.close();
+
+    return 0;
+}
+
+//------trash area------//
+/*
+ while (mainwindow.isOpen()){//the main game loop
         mainwindow.setPosition(Vector2i(283,0));
         Event event;
-        while (mainwindow.pollEvent(event)){//event collecting loop
-            switch(event.type){
+        while (mainwindow.pollEvent(event))//event collecting loop
+            switch(event.type){//Switch case for even handling
                 case Event::Closed:
                     mainwindow.close();
                     cout<<"Close the window"<<endl;
                     break;
-                
-            }
 
-        }
+            }
 
         mainwindow.display();
     }
-
-    return 0;
-}
+ */
