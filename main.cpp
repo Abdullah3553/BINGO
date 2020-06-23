e6ef66f7e59f8087caf363aca6f5aba3c12dac1b
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2020-06-23 14:31:23 +0200

    Start Window V1 Complete

--- main.cpp
+++ main.cpp
@@ -7,18 +7,16 @@
 using namespace std;
 using namespace sf;
 
-void Show_Error_Window(){
+void Show_Error_Window(Font &font){
     RenderWindow Error_Window(VideoMode(350, 200), "Warning!!", Style::Close);
     Error_Window.setPosition(Vector2i(150, 100));
 
-    sf::Font font;
     Text text, text2;
-    font.loadFromFile("ARLRDBD.TTF");
     //------ Text(Sentence) section --------//
     text.setPosition({45, 90});
     text.setStyle(sf::Text::Bold);
     text.setCharacterSize(20);
-    text.setColor(sf::Color::White);
+    text.setFillColor(sf::Color::White);
     text.setString("Choose the difficulty first.");
     text.setFont(font);
     //------ End_OF_Text section ------//
@@ -28,7 +26,7 @@ void Show_Error_Window(){
     text2.setCharacterSize(25);
     text2.setStyle(sf::Text::Bold);
     text2.setFont(font);
-    text2.setColor(sf::Color::White);
+    text2.setFillColor(sf::Color::White);
     text2.setString("Warning !!\n");
     //------- End_Of_Text2 section --------//
 
@@ -46,18 +44,17 @@ void Show_Error_Window(){
     while(Error_Window.isOpen()){
         Event event;
         while(Error_Window.pollEvent(event)){
-            if(event.type == sf::Event::Closed)
-                Error_Window.close();
-             if(event.type == sf::Event::MouseButtonPressed){
+
+            if(event.type == sf::Event::MouseButtonPressed){
                 if(Ok_btn.mosuein(Error_Window)){
                     Ok_btn.setScale({0.9, 0.9});
                 }
             }else if(event.type == sf::Event::MouseButtonReleased){
-                 if(Ok_btn.mosuein(Error_Window)){
-                     Ok_btn.setScale({1, 1});
-                     Error_Window.close();
-                 }
-             }
+                if(Ok_btn.mosuein(Error_Window)){
+                    Ok_btn.setScale({1, 1});
+                    Error_Window.close();
+                }
+            }
         }
         Error_Window.clear();
         Error_Window.draw(text);
@@ -66,20 +63,92 @@ void Show_Error_Window(){
         Error_Window.display();
     }
 }
+
+string nameEnter(Font &font,string player_name){//A function to take the user enter his/her name as an input
+    /*--------text box data elements---------*/
+    RenderWindow txtbox(VideoMode(400,100),"Enter the Name",Style::Close);
+    Textbox name(27,Color::White,true,false);
+    Text t1;
+    Vector2i center(350-200,VideoMode::getDesktopMode().height/2 - 50);
+    Texture ok_tex,cancel_tex;
+    //-------Texture area---------//
+    ok_tex.setSmooth(true);
+    cancel_tex.setSmooth(true);
+    ok_tex.loadFromFile("ok_btn.png");
+    cancel_tex.loadFromFile("cancel_btn.png");
+    //-------Texture area---------//
+
+    Button ok(&ok_tex),cancel(&cancel_tex);
+    /*--------text box data elements---------*/
+
+    ok.setPosition({347, 75});
+    cancel.setPosition({272,75});
+    t1.setFont(font);
+    t1.setFillColor(Color::White);
+    t1.setString("Enter Your Name :");
+    t1.setCharacterSize(18);
+    t1.setPosition({0,0});
+    txtbox.setPosition(center);
+    name.setPosition({0,25});
+    name.setFont(font);
+    name.setLimit(true,20);
+    while(txtbox.isOpen()){
+        Event e;
+        while(txtbox.pollEvent(e))
+            if(e.type==Event::TextEntered)
+                name.typedOn(e);
+            else if(e.type==Event::MouseButtonPressed)
+            {
+                cout<<Mouse::getPosition(txtbox).x<<" "<<Mouse::getPosition(txtbox).y<<endl;
+                if(ok.mosuein(txtbox)){
+                    txtbox.close();
+                }
+                else if(cancel.mosuein(txtbox)){
+                    return player_name;
+                }
+            }
+        txtbox.clear(Color(127,127,127));
+        txtbox.draw(t1);
+        name.drawTo(txtbox);
+        ok.drawTo(txtbox);
+        cancel.drawTo(txtbox);
+        txtbox.display();
+    }
+    player_name =name.getText();
+    return player_name;
+}
+
 int main(){
+    //------------Global Data-----------//
+    string player_name;
+    int difficulty;
+    //------------Global Data-----------//
+
     //-------- start window data -------//
     RenderWindow start_window(VideoMode(700,700),"Start Window",Style::Close);
     start_window.setPosition(Vector2i(0,0));
     Texture background_texture,start_btn_texture;
-//    background_texture.loadFromFile("full_window.jpg");// to test positions
+    // background_texture.loadFromFile("full_window.jpg");// to test positions
     background_texture.loadFromFile("background.jpg");
     start_btn_texture.loadFromFile("start_btn.png");
+    /*--------Texture area------*/
+
+    /*--------Data area------*/
     RectangleShape background;
+    Button start_btn(&start_btn_texture);
+    Font font;
+    bool isstart=false , error=false;
+    /*--------Data area------*/
+    //-------- start window data -------//
+
+    /*-------- Setting up data elements -------*/
+    start_window.setPosition(Vector2i(0,0));
+    //background_texture.loadFromFile("full_window.jpg");// to test positions
     background.setTexture(&background_texture);
     background.setPosition(0,0);
     background.setSize(Vector2f(background_texture.getSize()));
-    Button start_btn(&start_btn_texture);
     start_btn.setPosition({240, 196});
+    font.loadFromFile("ARLRDBD.TTF");
 //    start_btn.setBackColor(Color(255,255,255,100));
 
     //----------- Easy_btn section ------------//
@@ -87,7 +156,7 @@ int main(){
     Easy_btn_Texture.loadFromFile("easy_btn_np.png");
     Button Easy_btn(&Easy_btn_Texture);
     Easy_btn.setPosition({31, 365});
-    bool Changed_Easy_byn = 0;
+    bool Changed_Easy_byn = false;
     //----------- End_Of_Easy_Btn section --------//
 
     // ---------- Medium_btn section ------------//
@@ -95,7 +164,7 @@ int main(){
     Medium_btn_Texture.loadFromFile("meduim_btn_np.png");
     Button Medium_btn(&Medium_btn_Texture);
     Medium_btn.setPosition({30, 480});
-    bool Changed_Medium_btn = 0;
+    bool Changed_Medium_btn = false;
     //---------- End_of_Medium_btn section ---------//
 
     //---------- Hard_brn section ----------------//
@@ -103,79 +172,82 @@ int main(){
     Hard_btn_Texture.loadFromFile("hard_btn_np.png");
     Button Hard_btn (&Hard_btn_Texture);
     Hard_btn.setPosition({30, 590});
-    bool Changed_Hard_btn = 0;
+    bool Changed_Hard_btn = false;
     //----------- End_Of_Hard_Btn section ----------//
-//    sf::SoundBuffer sb;
-//    sb.loadFromFile("Error.wav");
-//    sf::Sound sound;
-//    sound.setBuffer(sb);
 
     while(start_window.isOpen()){//strat window loop
         Event ev;
+        if(isstart){//to check if Start button is pressed or not
+            isstart=false;
+            player_name = nameEnter(font,player_name);
+            if(!player_name.empty())start_window.close();
+        }else if(error){
+            Show_Error_Window(font);
+            error =false;
+        }
+
         while(start_window.pollEvent(ev))
             switch (ev.type) {//even handle switch case
-            case Event::Closed:
-                start_window.close();break;
-            case Event::MouseButtonPressed://to know the positions on the screen
-                cout<<Mouse::getPosition(start_window).x<<" "<<Mouse::getPosition(start_window).y<<endl;
-                if(start_btn.mosuein(start_window)) {
-                    if(!Changed_Hard_btn && !Changed_Medium_btn && !Changed_Easy_byn) {
-//                        sound.play();
-                        Show_Error_Window();
-                    }
-                    else {
-                        start_btn.setScale({0.9, 0.9});
-                    }
-                }
-                else if(Easy_btn.mosuein(start_window)) {
-                    if (!Changed_Hard_btn && !Changed_Medium_btn)
-                        Easy_btn.setScale({0.9, 0.9});
-                }
-                else if(Medium_btn.mosuein(start_window)) {
-                    if(!Changed_Easy_byn && !Changed_Hard_btn)
-                        Medium_btn.setScale({0.9, 0.9});
-                }
-                else if(Hard_btn.mosuein(start_window)){
-                    if(!Changed_Easy_byn && !Changed_Medium_btn)
-                        Hard_btn.setScale({0.9, 0.9});
-                }
-                break;
-            case Event::MouseButtonReleased:
-                if(start_btn.mosuein(start_window))
-                    start_btn.setScale({1,1});
-                else if(Easy_btn.mosuein(start_window)) {
-                    if(!Changed_Easy_byn && !Changed_Medium_btn && !Changed_Hard_btn) {
-                        Easy_btn.setScale({1, 1});
-                        Easy_btn_Texture.loadFromFile("easy_btn_p.png");
-                        Changed_Easy_byn = 1;
-                    }else{
-                        Easy_btn.setScale({1, 1});
-                        Easy_btn_Texture.loadFromFile("easy_btn_np.png");
-                        Changed_Easy_byn = 0;
-                    }
-                }else if(Medium_btn.mosuein(start_window)){
-                    if(!Changed_Medium_btn && !Changed_Hard_btn && !Changed_Easy_byn){
-                        Medium_btn.setScale({1, 1});
+                case Event::Closed:
+                    start_window.close();return 0;break;
+                case Event::MouseButtonPressed://to know the positions on the screen
+                    cout<<Mouse::getPosition(start_window).x<<" "<<Mouse::getPosition(start_window).y<<endl;
+                    if(start_btn.mosuein(start_window))
+                        start_btn.setScale({0.9,0.9});
 
-                        Medium_btn_Texture.loadFromFile("meduim_btn_p.png");
-                        Changed_Medium_btn = 1;
-                    } else{
-                        Medium_btn.setScale({1, 1});
-                        Medium_btn_Texture.loadFromFile("meduim_btn_np.png");
-                        Changed_Medium_btn = 0;
+                    else if(Easy_btn.mosuein(start_window)) {
+                        if (!Changed_Hard_btn && !Changed_Medium_btn)
+                            Easy_btn.setScale({0.9, 0.9});
                     }
-                }else if(Hard_btn.mosuein(start_window)){
-                    if(!Changed_Hard_btn && !Changed_Medium_btn && !Changed_Easy_byn){
-                        Hard_btn.setScale({1, 1});
-                        Hard_btn_Texture.loadFromFile("hard_btn_p.png");
-                        Changed_Hard_btn = 1;
-                    }else{
-                        Hard_btn.setScale({1, 1});
-                        Hard_btn_Texture.loadFromFile("hard_btn_np.png");
-                        Changed_Hard_btn = 0;
+                    else if(Medium_btn.mosuein(start_window)) {
+                        if(!Changed_Easy_byn && !Changed_Hard_btn)
+                            Medium_btn.setScale({0.9, 0.9});
                     }
-                }
-                break;
+                    else if(Hard_btn.mosuein(start_window)){
+                        if(!Changed_Easy_byn && !Changed_Medium_btn)
+                            Hard_btn.setScale({0.9, 0.9});
+                    }
+                    break;
+                case Event::MouseButtonReleased:
+                    if(start_btn.mosuein(start_window)) {
+                        start_btn.setScale({1, 1});
+                        if(Changed_Easy_byn||Changed_Hard_btn||Changed_Medium_btn)
+                            isstart=true;
+                        else
+                            error =true;}
+                    else if(Easy_btn.mosuein(start_window)) {
+                        if(!Changed_Easy_byn && !Changed_Medium_btn && !Changed_Hard_btn) {
+                            Easy_btn.setScale({1, 1});
+                            Easy_btn_Texture.loadFromFile("easy_btn_p.png");
+                            Changed_Easy_byn = true;
+                        }else{
+                            Easy_btn.setScale({1, 1});
+                            Easy_btn_Texture.loadFromFile("easy_btn_np.png");
+                            Changed_Easy_byn = false;
+                        }
+                    }else if(Medium_btn.mosuein(start_window)){
+                        if(!Changed_Medium_btn && !Changed_Hard_btn && !Changed_Easy_byn){
+                            Medium_btn.setScale({1, 1});
+
+                            Medium_btn_Texture.loadFromFile("meduim_btn_p.png");
+                            Changed_Medium_btn = true;
+                        } else{
+                            Medium_btn.setScale({1, 1});
+                            Medium_btn_Texture.loadFromFile("meduim_btn_np.png");
+                            Changed_Medium_btn = false;
+                        }
+                    }else if(Hard_btn.mosuein(start_window)){
+                        if(!Changed_Hard_btn && !Changed_Medium_btn && !Changed_Easy_byn){
+                            Hard_btn.setScale({1, 1});
+                            Hard_btn_Texture.loadFromFile("hard_btn_p.png");
+                            Changed_Hard_btn = true;
+                        }else{
+                            Hard_btn.setScale({1, 1});
+                            Hard_btn_Texture.loadFromFile("hard_btn_np.png");
+                            Changed_Hard_btn = false;
+                        }
+                    }
+                    break;
             }
         start_window.draw(background);
         start_btn.drawTo(start_window);
@@ -184,6 +256,11 @@ int main(){
         Hard_btn.drawTo(start_window);
         start_window.display();
     }
+    /*--------select difficulty---------*/
+    if(Changed_Easy_byn)difficulty=1;
+    else if(Changed_Medium_btn)difficulty=2;
+    else difficulty =3;
+    /*--------select difficulty---------*/
 //RenderWindow mainwindow(VideoMode(800, 700), "PENGO", Style::Close);
     //mainwindow.close();
 
