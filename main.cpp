c05c40c44356069bf53c7171e66fd6a805889063
AhmedGamal <ahmedgamale.1019@gmail.com>
2020-06-23 13:22:37 +0200

    Adding Error window

--- main.cpp
+++ main.cpp
@@ -1,21 +1,70 @@
 /*---------BINGO---------*/
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include<iostream>
 #include"Button.h"
-
+#include "Textbox.h"
 using namespace std;
 using namespace sf;
 
 void Show_Error_Window(){
-    RenderWindow Error_Window(VideoMode(200, 200), "Warning!!", Style::Close);
+    RenderWindow Error_Window(VideoMode(350, 200), "Warning!!", Style::Close);
     Error_Window.setPosition(Vector2i(150, 100));
-    Error_Window.setSize(sf::Vector2u(100, 100));
-    Error_Window.setTitle("Warning!!");
-    
-    sf::Font font;
-    Text text;
-    font.loadFromFile("ARLRDBD.TTF");
 
+    sf::Font font;
+    Text text, text2;
+    font.loadFromFile("ARLRDBD.TTF");
+    //------ Text(Sentence) section --------//
+    text.setPosition({45, 90});
+    text.setStyle(sf::Text::Bold);
+    text.setCharacterSize(20);
+    text.setColor(sf::Color::White);
+    text.setString("Choose the difficulty first.");
+    text.setFont(font);
+    //------ End_OF_Text section ------//
+
+    //------ Text2(Warning ward) Section ------//
+    text2.setPosition({110, 40});
+    text2.setCharacterSize(25);
+    text2.setStyle(sf::Text::Bold);
+    text2.setFont(font);
+    text2.setColor(sf::Color::White);
+    text2.setString("Warning !!\n");
+    //------- End_Of_Text2 section --------//
+
+    //-------- Texture --------//
+    Texture Ok_btn_Texture;
+    Ok_btn_Texture.loadFromFile("Ok_btn.png");
+    Ok_btn_Texture.setSmooth(true);
+    //------- EndOfTexture-------//
+
+    /*------- Ok_Button Section ----------*/
+    Button Ok_btn(&Ok_btn_Texture);
+    Ok_btn.setPosition({145, 150});
+    /*------End_Of_Ok_Button section------*/
+
+    while(Error_Window.isOpen()){
+        Event event;
+        while(Error_Window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+                Error_Window.close();
+             if(event.type == sf::Event::MouseButtonPressed){
+                if(Ok_btn.mosuein(Error_Window)){
+                    Ok_btn.setScale({0.9, 0.9});
+                }
+            }else if(event.type == sf::Event::MouseButtonReleased){
+                 if(Ok_btn.mosuein(Error_Window)){
+                     Ok_btn.setScale({1, 1});
+                     Error_Window.close();
+                 }
+             }
+        }
+        Error_Window.clear();
+        Error_Window.draw(text);
+        Error_Window.draw(text2);
+        Ok_btn.drawTo(Error_Window);
+        Error_Window.display();
+    }
 }
 int main(){
     //-------- start window data -------//
@@ -56,9 +105,12 @@ int main(){
     Hard_btn.setPosition({30, 590});
     bool Changed_Hard_btn = 0;
     //----------- End_Of_Hard_Btn section ----------//
+//    sf::SoundBuffer sb;
+//    sb.loadFromFile("Error.wav");
+//    sf::Sound sound;
+//    sound.setBuffer(sb);
 
     while(start_window.isOpen()){//strat window loop
-
         Event ev;
         while(start_window.pollEvent(ev))
             switch (ev.type) {//even handle switch case
@@ -66,8 +118,15 @@ int main(){
                 start_window.close();break;
             case Event::MouseButtonPressed://to know the positions on the screen
                 cout<<Mouse::getPosition(start_window).x<<" "<<Mouse::getPosition(start_window).y<<endl;
-                if(start_btn.mosuein(start_window))
-                    start_btn.setScale({0.9,0.9});
+                if(start_btn.mosuein(start_window)) {
+                    if(!Changed_Hard_btn && !Changed_Medium_btn && !Changed_Easy_byn) {
+//                        sound.play();
+                        Show_Error_Window();
+                    }
+                    else {
+                        start_btn.setScale({0.9, 0.9});
+                    }
+                }
                 else if(Easy_btn.mosuein(start_window)) {
                     if (!Changed_Hard_btn && !Changed_Medium_btn)
                         Easy_btn.setScale({0.9, 0.9});
