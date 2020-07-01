93d1ac7f1f62d2b26eabb9202cc6c0f0186a2b0a
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2020-07-01 11:52:15 +0200

    ....

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/lenovo/Documents/Clion Projects/BINGO_lod3
+# Install script for directory: C:/Users/lenovo/Documents/Clion Projects/BINGO
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/lenovo/Documents/Clion Projects/BINGO_lod3/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/lenovo/Documents/Clion Projects/BINGO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
