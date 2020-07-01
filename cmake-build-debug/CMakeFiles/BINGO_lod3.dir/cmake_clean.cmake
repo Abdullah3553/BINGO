89f1a74a4006290f463ba296b3f3bb84f7621a20
Abdullah Mahmoud <49757373+Abdullah3553@users.noreply.github.com>
2020-07-01 11:18:55 +0200

    Edit Ignore File + tst 2

---
+++ cmake-build-debug/CMakeFiles/BINGO_lod3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BINGO_lod3.exe"
+  "BINGO_lod3.exe.manifest"
+  "BINGO_lod3.pdb"
+  "CMakeFiles/BINGO_lod3.dir/main.cpp.obj"
+  "libBINGO_lod3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BINGO_lod3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BINGO_lod3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BINGO_lod3.
+# This may be replaced when dependencies are built.
